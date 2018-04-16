27c1bb6b4aa79a54b2b7210a99a6061db0bb7e85
Ben Pirt <ben@pirt.co.uk>
2018-04-16 14:21:46 +0100

    Add user model and get sign up working

--- db/migrate/20180416112844_devise_create_users.rb
+++ db/migrate/20180416112844_devise_create_users.rb
@@ -31,7 +31,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      
+      t.string :name, null: false
+      t.float :location_lat, null: false
+      t.float :location_lon, null: false
+      t.integer :max_party_size
+      t.integer :price_per_head
 
       t.timestamps null: false
     end
