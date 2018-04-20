c4c8ea8612aec949f2b0439331e345f6bb9ff4bf
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-04-20 15:36:00 +0100

    created cuisine model, jointable and select checkbox in view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416112844) do
+ActiveRecord::Schema.define(version: 20180420131539) do
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "food"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cuisines_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "cuisine_id", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
